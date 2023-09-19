fa658bf10a4d84bfb0e7dcb23ac402abffb302cb
roi <roi14102000@gmail.com>
2023-09-19 10:31:43 +0300

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import './css/App.css'
-import { useContext } from 'react'
-import { test_contexts } from './Context'
-import Layout from './components/layout'
-import Home from './Pages/Home'
-
+import "./css/App.css";
+import { useContext } from "react";
+import { test_contexts } from "./Context";
+import Layout from "./components/layout";
+import Home from "./Pages/Home";
+import DegreePage from "./components/DegreePage";
+import { Register } from "./Pages/Register";
+import { Payment } from "./Pages/Payment";
 
 function App() {
   const test = useContext(test_contexts);
 
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-      <Route path="/" element={<Home/>}/>
-    
-      </Route>
-
-
-    </Routes>
-
-    
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/degree/:degree" element={<DegreePage />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/payment" element={<Payment />} />
+        </Route>
+      </Routes>
     </>
   );
 }
